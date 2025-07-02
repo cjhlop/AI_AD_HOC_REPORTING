c9f9b232fa3b8425c25c855f30ab65197ac0884c
[dyad] <git@dyad.sh>
2025-07-02 14:15:23 +0300

    Reverted all changes back to version a553ed11a759cdc111099c299882d8f9adb0d8c6

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import AIChat from "./pages/AIChat";
-import AIResponse from "./pages/AIResponse";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -19,7 +18,6 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/ai-chat" element={<AIChat />} />
-          <Route path="/ai-chat/response" element={<AIResponse />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
