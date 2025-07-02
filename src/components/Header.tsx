0077e9ca1fb66283595465907921ddd846a6fb22
[dyad] <git@dyad.sh>
2025-07-02 12:20:35 +0300

    [dyad] Adjusted header width - wrote 1 file(s)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,7 @@ import {
 
 const Header = () => {
   return (
-    <header className="bg-white border-b border-gray-200 px-6 py-4 ml-64">
+    <header className="bg-white border-b border-gray-200 px-6 py-4">
       <div className="flex items-center justify-end">
         {/* Right side - Actions and Profile */}
         <div className="flex items-center space-x-4">
